a83354a1ff3f81a2a8d1ebc73d6bc08434ed99b6
y-jamoure <youssef.jamoure-ext@cdgcapital.ma>
2020-11-23 14:47:10 +0100

    init prj

---
+++ src/test/java/com/example/gitActions/GitActionsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.gitActions;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsApplicationTests {
+
+  @Test
+  void unitTestExample() {
+    Assert.assertNull(null);
+  }
+}
